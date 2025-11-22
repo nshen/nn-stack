5bf563bd3c1b31238cf5fcfc2e0d4b0b8cbec064
Xin Chen <nshen121@gmail.com>
2025-11-22 12:23:50 +0800

    feat: add nextjs

---
+++ apps/web/alchemy.run.ts
@@ -0,0 +1,12 @@
+import alchemy from 'alchemy';
+import { Nextjs } from 'alchemy/cloudflare';
+
+const app = await alchemy('nn-stack');
+
+export const web = await Nextjs('web', {
+  name: `${app.name}-web-${app.stage}`
+});
+
+console.log({ url: web.url });
+
+await app.finalize();
