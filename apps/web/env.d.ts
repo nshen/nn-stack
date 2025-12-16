8579aca03bc8ff38e514597c4f8ac7b5823a50f7
Xin Chen <nshen121@gmail.com>
2025-12-17 01:19:52 +0800

    fix: fix web env type

--- apps/web/env.d.ts
+++ apps/web/env.d.ts
@@ -2,11 +2,12 @@
 // @see https://alchemy.run/concepts/bindings/#type-safe-bindings
 
 import type { web } from './alchemy.run.ts';
+import type { CloudflareEnv as ApiEnv } from '@nn-stack/api/env';
 
 export type WebEnv = typeof web.Env;
 
 declare module 'cloudflare:workers' {
   namespace Cloudflare {
-    export interface Env extends WebEnv {}
+    export interface Env extends WebEnv, ApiEnv {}
   }
 }
